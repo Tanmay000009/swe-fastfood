701f304ac492bfc814ffa0497f562390211a9ace
Tanmay000009 <tanmayvyas009@gmail.com>
2022-11-05 17:27:22 +0530

    create order model

---
+++ models/Order.js
@@ -0,0 +1,49 @@
+const { default: mongoose } = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+  orderItems: [
+    {
+      item: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "MenuItem",
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  restaurantId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Restaurant",
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  orderStatus: {
+    type: String,
+    required: true,
+    default: "Awaiting Confirmation",
+  },
+  orderTotal: {
+    type: Number,
+    required: true,
+  },
+  createdDate: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  updatedDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;
